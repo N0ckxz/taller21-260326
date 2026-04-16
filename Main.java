01e8d3b9176ece904727a0dc52ea22ca44105e33
Noxz <elqueni@hotmail.com>
2026-04-16 18:48:41 -0500

    Implementacion de logica principal y algoritmo de la suma de Gauss

--- Main.java
+++ Main.java
@@ -6,9 +6,16 @@ public class Main {
 
         int n = sc.nextLong();
 
-        int sumaT = n * (n + 1) / 2;
+        int sumaT = n * (n + 1) / 2; //Algoritmo de suma de Gauss, referencia: https://nkhilv.medium.com/using-gauss-summation-to-add-numbers-quicker-2ab61d01eff1 
 
         int sumaR = 0;
 
+        for (int i = 0; i <= n - 1; i++) { 
+            sumaReal += sc.nextLong();
+        }
+
+        int numeroFaltante = sumaTeorica - sumaReal;
+        
+        System.out.println(numeroFaltante);
     }
 }
