a1a03a35190f5bce5cf65fbe60a0bc6fc63cfb89
Noxz <elqueni@hotmail.com>
2026-04-16 18:54:19 -0500

    Correccion de nombres de variables y de logica para el ciclo

--- Main.java
+++ Main.java
@@ -6,15 +6,15 @@ public class Main {
 
         long n = sc.nextLong();
 
-        long sumaT = n * (n + 1) / 2; //Algoritmo de suma de Gauss, referencia: https://nkhilv.medium.com/using-gauss-summation-to-add-numbers-quicker-2ab61d01eff1 
+        long sumaT = (long) n * (n + 1) / 2; //Algoritmo de suma de Gauss, referencia: https://nkhilv.medium.com/using-gauss-summation-to-add-numbers-quicker-2ab61d01eff1 
 
         long sumaR = 0;
 
-        for (long i = 0; i <= n - 1; i++) { 
-            sumaReal += sc.nextLong();
+        for (long i = 0; i < n - 1; i++) { //Ciclo para leer n-1 elementos
+            sumaR += sc.nextLong();
         }
 
-        long numeroFaltante = sumaTeorica - sumaReal;
+        long numeroFaltante = sumaT - sumaR;
         
         System.out.println(numeroFaltante);
     }
